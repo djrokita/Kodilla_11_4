ddbd2e20714afb903e2bf4c90983b309b7567a1a
Krzych <krzych.pierzchala@gmail.com>
2017-10-11 17:26:54 +0200

    Project ready to push

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+	this.printInfo = function() {
+		console.log('The phone brand is ' + this.brand +', color is ' + this.color + ' and the price is ' + this.price +'.');
+	};
+}
+
+var iPhoneS6 = new Phone('Apple', 4500, 'white');
+var samsungGalaxy6S = new Phone('Samsung', 2999, 'black');
+var onePlusOne = new Phone('OnePlus', 2499, 'silver');
+var kodillaPhone = new Phone('Kodilla', 10295, 'green');
+var xperiaXZ = new Phone('Sony', 2899, 'gray');
+var googlePixel = new Phone('Google', 3999, 'white');
+
+/*Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand +", color is " + this.color + " and the price is " + this.price +".");
+}*/
+
+iPhoneS6.printInfo();
+samsungGalaxy6S.printInfo();
+onePlusOne.printInfo();
+kodillaPhone.printInfo();
+xperiaXZ.printInfo();
+googlePixel.printInfo();
+
+Phone.prototype.category = 'premium';
+
+console.log(kodillaPhone.category);
+
+Phone.prototype.canIAfford = function () {
+	if (this.price > 3000) console.log("I can't afford " + this.brand);
+	else console.log("I'll buy " + this.brand);
+};
+
+googlePixel.canIAfford();
+onePlusOne.canIAfford();
